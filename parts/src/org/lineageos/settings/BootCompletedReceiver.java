cff1df234d88188349e97b8cebee4b139f1779a9
Mudit200408 <muditpurohit2004.pm@gmail.com>
2023-03-25 22:35:01 +0530

    Revert "Add DC Dimming"
    This reverts commit f5b48f44e8df9986fab67a474db30c83bdae0ed0.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -20,10 +20,8 @@ package org.lineageos.settings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.SystemProperties;
 import android.util.Log;
-import androidx.preference.PreferenceManager;
+
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.popupcamera.PopupCameraUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
@@ -34,46 +32,14 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     private static final boolean DEBUG = false;
     private static final String TAG = "XiaomiParts";
-    private static final String DC_DIMMING_ENABLE_KEY = "dc_dimming_enable";
-    private static final String DISPPARAM_NODE = "/sys/class/drm/card0-DSI-1/disp_param";
-
-    private static final String DISPPARAM_DC_ON = "0x40000";
-    private static final String DISPPARAM_DC_OFF = "0x50000";
-    private static final String DISPPARAM_DIMMING_ON = "0xF00";
-    private static final String DISPPARAM_DIMMING_OFF = "0xE00";
-    private static final String DISPPARAM_CRC_OFF = "0xF00000";
-
-    private static final String BRIGHTNESS_NODE = "/sys/class/backlight/panel0-backlight/brightness";
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
-            
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DozeUtils.checkDozeService(context);
         PopupCameraUtils.checkPopupCameraService(context);
         ThermalUtils.startService(context);
         FileUtils.enableService(context);
         RefreshUtils.startService(context);  
-        
-        boolean dcDimmingEnabled = sharedPrefs.getBoolean(DC_DIMMING_ENABLE_KEY, false);
-        setDcDimmingStatus(dcDimmingEnabled);
-    }
-
-    void setDcDimmingStatus(boolean enabled) {
-        if (enabled) {
-            FileUtils.writeLine(DISPPARAM_NODE, DISPPARAM_DC_ON);
-            FileUtils.writeLine(DISPPARAM_NODE, DISPPARAM_DIMMING_OFF);
-            FileUtils.writeLine(DISPPARAM_NODE, DISPPARAM_DIMMING_ON);
-            // Update the brightness node so dc dimming updates its state
-            FileUtils.writeLine(BRIGHTNESS_NODE, FileUtils.readOneLine(BRIGHTNESS_NODE));
-        } else {
-            FileUtils.writeLine(DISPPARAM_NODE, DISPPARAM_DIMMING_OFF);
-            FileUtils.writeLine(DISPPARAM_NODE, DISPPARAM_CRC_OFF);
-            FileUtils.writeLine(DISPPARAM_NODE, DISPPARAM_DC_OFF);
-            FileUtils.writeLine(DISPPARAM_NODE, DISPPARAM_DIMMING_ON);
-            // Update the brightness node so dc dimming updates its state
-            FileUtils.writeLine(BRIGHTNESS_NODE, FileUtils.readOneLine(BRIGHTNESS_NODE));
-        }
     }
 }
